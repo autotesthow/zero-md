b17fa99c2f5ee71db1f3a7cbf986d972177f5de6
nikalz <bynikalz@gmail.com>
2023-09-14 17:39:55 +0300

    fix: rename tests

--- test/unit-tests/index.spec.js
+++ test/unit-tests/index.spec.js
@@ -5,7 +5,7 @@ import unitTests from './unit-tests.spec.js'
 import customTabNamesTests from './custom-tab-names.spec.js'
 import attributeTests from './attributes-tests.spec.js'
 import tocTests from './toc-tests.spec.js'
-import variablesTests from './variables-tests.spec.js'
+import translationsTests from './translations-tests.spec.js'
 import localImportTests from './local-import-tests.spec.js'
 import onBoardTests from './on-board-tests.spec.js'
 
@@ -14,7 +14,7 @@ unitTests()
 customTabNamesTests()
 attributeTests()
 tocTests()
-variablesTests()
+translationsTests()
 localImportTests()
 onBoardTests()
 
